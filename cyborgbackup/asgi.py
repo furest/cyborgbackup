7c1d6335dbe947d85bf476507bff9998c62de3a7
furest <furest@furest.be>
2025-01-27 22:01:09 +0100

    fix daphne not starting

--- cyborgbackup/asgi.py
+++ cyborgbackup/asgi.py
@@ -5,17 +5,16 @@ from channels.routing import ProtocolTypeRouter, URLRouter
 from django.core.asgi import get_asgi_application
 from django.urls import re_path
 
+django_asgi_app = get_asgi_application()
+
 from cyborgbackup import prepare_env
 from cyborgbackup.main.consumers import CyBorgBackupConsumer, JwtAuthMiddlewareStack
-
-django_asgi_app = get_asgi_application()
 
 prepare_env()
 
 logger = logging.getLogger('cyborgbackup.asgi')
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "cyborgbackup.settings")
 
-django_asgi_app = get_asgi_application()
 
 application = ProtocolTypeRouter({
     "http": django_asgi_app,
